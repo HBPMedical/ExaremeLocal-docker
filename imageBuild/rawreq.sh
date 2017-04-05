25ece7166c4c4f5ad7b42e56d760e7a27b6c70a0
Yannis Chronis <johnchronis@gmail.com>
2017-04-05 17:31:54 +0300

    raw-ui name

--- imageBuild/rawreq.sh
+++ imageBuild/rawreq.sh
@@ -1,3 +1,4 @@
 wget --header="Authorization: Basic ZmVkZXJhdGlvbjpmZWRlcmF0aW9u" --header="Content-Type: application/json;charset=UTF-8" \
 --post-data="{\"query\": \"select distinct patient_id as __rid, variable_name as __colname, value as __val from exam_value\"}" \
-rawdb:54321/query
+raw-ui:5555/query
+
